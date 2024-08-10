a3cee0a833f564ff6ff18d423dce396c9be9795a
Yusuf YILDIRIM <122603576+MYusufY@users.noreply.github.com>
2024-08-10 23:10:49 +0300

    inital commit 2

--- test-dist/test.py
+++ test-dist/test.py
@@ -13,10 +13,19 @@ white = (255, 255, 255)
 black = (0, 0, 0)
 
 font = pygame.font.Font(None, 150)
-text = font.render("TEST", True, black)
+text = font.render("TestOS", True, black)
 text_rect = text.get_rect(center=(screen_width//2, screen_height//2))
 
 while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+
     screen.fill(white)
 
     screen.blit(text, text_rect)
